7c7a8dc61011deda8e3d4703e78e048ca36cbd1a
Ajit <ajit110920@gmail.com>
2024-06-07 21:13:47 +0200

    helpers

---
+++ 02_02_begin/helpers.py
@@ -0,0 +1,32 @@
+"""
+Python Data Structures and Algorithms - A Game-Based Approach
+Helper functions and values for use with other files in this project.
+"""
+
+offsets = {
+    "right": (0, 1),
+    "left": (0, -1),
+    "up": (-1, 0),
+    "down": (1, 0)
+}
+
+
+
+
+
+def is_legal_pos(maze, pos):
+    i, j = pos
+    num_rows = len(maze)
+    num_cols = len(maze[0])
+    return 0 <= i < num_rows and 0 <= j < num_cols and maze[i][j] != "*"
+
+
+def get_path(predecessors, start, goal):
+    current = goal
+    path = []
+    while current != start:
+        path.append(current)
+        current = predecessors[current]
+    path.append(start)
+    path.reverse()
+    return path
