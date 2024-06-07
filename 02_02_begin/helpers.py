b2fd97818ad9f94ac36a7c577db4f1a96ac87486
Ajit <ajit110920@gmail.com>
2024-06-07 21:14:16 +0200

    helpers_read_maze

--- 02_02_begin/helpers.py
+++ 02_02_begin/helpers.py
@@ -12,6 +12,22 @@ offsets = {
 
 
 
+def read_maze(file_name):
+    """
+    Reads a maze stored in a text file and returns a 2d list containing the maze representation.
+    """
+    try:
+        with open(file_name, "r", newline=None) as fh:
+            maze = [[char for char in line.strip("\r\n")] for line in fh]
+            num_cols_top_row = len(maze[0])
+            for row in maze:
+                if len(row) != num_cols_top_row:
+                    print("The maze is not rectangular.")
+                    raise SystemExit
+            return maze
+    except IOError:
+        print("There is a problem with the file you have selected.")
+        raise SystemExit
 
 
 def is_legal_pos(maze, pos):
