f3dc67d5bce8bab105ddb222672a02d876b2f7f8
Ajit <ajit110920@gmail.com>
2024-06-10 18:40:44 +0200

    reverse string

---
+++ 02_02_begin/reverse_string.py
@@ -0,0 +1,14 @@
+"""
+Python Data Structures - A Game-Based Approach
+Stack challenge
+"""
+
+import stack
+
+string = "gninraeL nIdekniL htiw tol a nraeL"
+reversed_string = ""
+s = stack.Stack()
+
+# Your solution here.
+
+print(reversed_string)
