c499d96d8571a166e8e680eb19b420854ec8c8b2
Ajit <ajit110920@gmail.com>
2024-06-30 21:32:41 +0200

    stack edit

--- 02_02_end/stack.py
+++ 02_02_end/stack.py
@@ -18,7 +18,14 @@ class Stack:
     def pop(self):
         return self.items.pop()
 
+    def peek(self):
+        return self.items[-1]
 
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
 
 
 if __name__ == "__main__":
