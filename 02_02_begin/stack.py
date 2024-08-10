544c456df3ad6dc2c88d3dee13065ac12eb6671e
Ajit <ajit110920@gmail.com>
2024-08-10 19:58:48 +0200

    feat(stack): add stack functionality

--- 02_02_begin/stack.py
+++ 02_02_begin/stack.py
@@ -5,4 +5,19 @@ Stack class
 
 
 class Stack:
-    pass
+    def __init__(self):
+        self._stack = []
+
+    def push(self, item):
+        self._stack.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self._stack.pop()
+        raise IndexError("pop from an empty stack")
+
+    def is_empty(self):
+        return len(self._stack) == 0
+
+    def __len__(self):
+        return len(self._stack)
