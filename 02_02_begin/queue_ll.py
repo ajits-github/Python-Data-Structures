5d1f7dc955499728fb0a024551dd444337d94329
Ajit <ajit110920@gmail.com>
2024-08-11 16:53:23 +0200

    feat(pq_2): add logic for pq 2

--- 02_02_begin/queue_ll.py
+++ 02_02_begin/queue_ll.py
@@ -7,4 +7,19 @@ from collections import deque
 
 
 class Queue:
-    pass
+    def __init__(self):
+        self._queue = deque()
+
+    def enqueue(self, item):
+        self._queue.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self._queue.popleft()
+        raise IndexError("dequeue from an empty queue")
+
+    def is_empty(self):
+        return len(self._queue) == 0
+
+    def __len__(self):
+        return len(self._queue)
